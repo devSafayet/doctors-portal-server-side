2466b0444ead1759a8566176fafd072b9e668740
Safayet <mrishafayet003@gmail.com>
2022-06-08 20:30:22 +0600

    collected mongodb to client side server

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.allbn.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db('doctors-portal').collection('services');
+    console.log('mongodb is connect');
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+
+  }
+  finally {
+
+  }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
